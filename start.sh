aebcb6b576b02513d8991c52e9d99fe8dc7cbd3b
Francesco Paolo Lovergine <frankie@debian.org>
2021-03-29 15:53:47 +0200

    Restore files at start time only if gluster dirs are empty.

--- start.sh
+++ start.sh
@@ -4,7 +4,13 @@ GLUSTERD_LOG_LEVEL=INFO
 GLUSTERD_LOG_FILE=/var/log/glusterfs/glusterd.log
 GLUSTERD_OPTIONS=
 
-cp -far /etc/glusterfs_bkp/* /etc/glusterfs/.
-cp -far /var/lib/glusterd_bkp/* /var/lib/glusterd/.
+for d in /etc/glusterfs /var/lib/glusterd
+do
+	if ! test "$(ls $d)"
+	then
+		cp -far ${d}_bkp/* ${d}/.
+	fi
+done
+
 /usr/sbin/glustereventsd --pid-file /run/glustereventsd.pid &
 /usr/sbin/glusterd --log-level=$GLUSTERD_LOG_LEVEL --log-file=$GLUSTERD_LOG_FILE --no-daemon $GLUSTERD_OPTIONS
