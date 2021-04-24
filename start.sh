66272a6cdc03aa11a20f7b3b2719b31a4abd0f97
Francesco Paolo Lovergine <frankie@debian.org>
2021-04-24 18:20:21 +0200

    Added logrotate and cron to manage automagically rotation of log files.

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ GLUSTERD_OPTIONS=
 cp -far /etc/glusterfs_bkp/* /etc/glusterfs/.
 cp -far /var/lib/glusterd_bkp/* /var/lib/glusterd/.
 /usr/sbin/glustereventsd --pid-file /run/glustereventsd.pid &
+/usr/sbin/cron &
 /usr/sbin/glusterd --log-level=$GLUSTERD_LOG_LEVEL --log-file=$GLUSTERD_LOG_FILE --no-daemon $GLUSTERD_OPTIONS
